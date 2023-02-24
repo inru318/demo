b50ad828adda18313cb02ba3d1af9e0b7f341c6e
inru <anny20258@gmail.com>
2023-02-24 23:39:04 +0800

    handel conflict

--- demo.py
+++ demo.py
@@ -1,9 +1,6 @@
 def info():
-<<<<<<< HEAD
     print("hello,I am freen,I like watch Netflix")
-=======
     print("hello, I am Becky,I like drink coffee")
->>>>>>> 0c0e67aef378bc6cd8adb59da6572d2eba31f835
 
 if __name__=='__main__':
     info()
